724945f536db6a633cd0bce92a8d5d1e8c42b566
hidetoshi.kanno <taf71811@docomo.ne.jp>
2018-06-30 12:24:39 +0900

    2018/6/30

--- Instagram/ImageSelectViewController.swift
+++ Instagram/ImageSelectViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ImageSelectViewController.swift
 //  Instagram
 //
 //  Created by 菅野 英俊 on 2018/06/28.
@@ -7,28 +7,24 @@
 //
 
 import UIKit
-import ESTabBarController
 
-class ViewController: UIViewController {
+class ImageSelectViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        setupTab()
-        
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
-    func setupTab() {
-        // 画像のファイル名を指定してESTabBarControllerを作成する
-        
-    }
 
 }
+
+
+
+
 
--- Instagram/SettingViewController.swift
+++ Instagram/SettingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SettingViewController.swift
 //  Instagram
 //
 //  Created by 菅野 英俊 on 2018/06/28.
@@ -7,28 +7,20 @@
 //
 
 import UIKit
-import ESTabBarController
 
-class ViewController: UIViewController {
+class SettingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        setupTab()
-        
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     
-    func setupTab() {
-        // 画像のファイル名を指定してESTabBarControllerを作成する
-        
-    }
+
 
 }
-
